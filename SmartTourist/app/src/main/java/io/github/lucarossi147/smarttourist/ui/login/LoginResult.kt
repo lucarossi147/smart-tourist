c33d08140c019d517af10b8f0bcc0de6323214e2
luca <lucarossi147@gmail.com>
2022-04-27 15:19:56 +0200

    added login and maps fragment

---
+++ SmartTourist/app/src/main/java/io/github/lucarossi147/smarttourist/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package io.github.lucarossi147.smarttourist.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
