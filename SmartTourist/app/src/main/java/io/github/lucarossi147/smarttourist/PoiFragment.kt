01ef67bf90bdf49a56b114ddf984d84d9bf9106a
luca <lucarossi147@gmail.com>
2022-05-01 13:09:40 +0200

    improved poi fragment

--- SmartTourist/app/src/main/java/io/github/lucarossi147/smarttourist/PoiFragment.kt
+++ SmartTourist/app/src/main/java/io/github/lucarossi147/smarttourist/PoiFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.findNavController
+import com.squareup.picasso.Picasso
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,6 +44,28 @@ class PoiFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_poi, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tv: TextView = view.findViewById(R.id.poiInfoTextView)
+        // TODO: fetch poi info
+        tv.text = resources.getString(R.string.loremIpsum)
+        val button: Button = view.findViewById(R.id.signButton)
+        button.setOnClickListener {
+            // TODO: send signature and comment to server
+            view.findNavController().navigate(R.id.mapsFragment)
+        }
+        for (i in 1..20){
+            val iv = ImageView(context)
+            val linearLayout: LinearLayout = view.findViewById(R.id.images_linear_layout)
+            linearLayout.addView(iv)
+            val r = Random.nextInt(500)
+            Picasso.get()
+                .load("https://placedog.net/$r")
+                .resize(0, 400)
+                .into(iv)
+        }
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
