d4c6490c9f2bbd9c746e0059ca377f8372657365
Luca Rossi <lucarossi147@gmail.com>
2022-05-18 16:48:20 +0200

    made constants const

--- SmartTourist/app/src/main/java/io/github/lucarossi147/smarttourist/Constants.kt
+++ SmartTourist/app/src/main/java/io/github/lucarossi147/smarttourist/Constants.kt
@@ -1,6 +1,6 @@
 package io.github.lucarossi147.smarttourist
 
 object Constants {
-    val AUTH_URL = "https://smart-tourist-cup3lszycq-uc.a.run.app/"
-    val POI_URL = "https://poi-service-container-cup3lszycq-uc.a.run.app/"
+    const val AUTH_URL = "https://smart-tourist-cup3lszycq-uc.a.run.app/"
+    const val POI_URL = "https://poi-service-container-cup3lszycq-uc.a.run.app/"
 }
