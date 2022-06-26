181ce851dd0212e13812bf4db08926de975f89c6
Davide Schiaroli <dados1998@hotmail.it>
2022-06-26 22:59:32 +0200

    Modified game-ms

--- microservices/game-ms/build.gradle.kts
+++ microservices/game-ms/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 application {
-    mainClass.set("com.example.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
